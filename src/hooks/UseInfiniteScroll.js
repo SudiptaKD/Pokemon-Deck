11d922b2eb9491521a95988ed7f623189e0ab1c8
Sudipta Kumar Dam <sudiptakumardam@gmail.com>
2021-09-13 02:39:20 +0600

    finally fixed for all screen by scrollHeight Element after 8 hours of searching and reading article. but didnt find there

--- src/hooks/UseInfiniteScroll.js
+++ src/hooks/UseInfiniteScroll.js
@@ -5,13 +5,27 @@ const useInfiniteScroll = (callback) => {
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
-    window.addEventListener('resize', handleScroll)
-    window.addEventListener('touchend', handleTouchEnd);
-    window.addEventListener('touchmove', handleTouchEnd);
+    window.addEventListener('resize', handleScroll);
+
+    //Scroll event didnt work in touch screen for inner height element. so tried touch event. this didn't work
+    // window.addEventListener('touchstart', handleTouchEnd);
+    // window.addEventListener('touchmove', handleTouchEnd);
+    // window.addEventListener('touchend', handleTouchEnd);
+    // window.addEventListener('mousemove', handleTouchEnd);
+    // window.addEventListener('mousedown', handleTouchEnd);
+    // window.addEventListener('mouseup', handleTouchEnd);
+    // window.addEventListener('click', handleTouchEnd);
+   
     return () =>{ window.removeEventListener('scroll', handleScroll);
-                  window.removeEventListener('resize', handleScroll)
-                  window.removeEventListener('touchend', handleTouchEnd)
-                  window.removeEventListener('touchmove', handleTouchEnd)
+                   window.removeEventListener('resize', handleScroll);
+
+            //       window.removeEventListener('touchstart', handleTouchEnd);
+            //       window.removeEventListener('touchmove', handleTouchEnd);
+            //       window.removeEventListener('touchend', handleTouchEnd);
+            //       window.removeEventListener('mousemove', handleTouchEnd);  
+            //       window.removeEventListener('mousedown', handleTouchEnd);  
+            //       window.removeEventListener('mouseup', handleTouchEnd);  
+            //       window.removeEventListener('click', handleTouchEnd);       
             }
   }, []);
 
@@ -23,19 +37,25 @@ const useInfiniteScroll = (callback) => {
   }, [isFetching]);
 
   function handleScroll() {
-    if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight || isFetching) return;
+    if (document.documentElement.scrollHeight - Math.abs(document.documentElement.scrollTop) <  //Finally worked on all screen
+       document.documentElement.clientHeight + 100) { 
+          setIsFetching(true); 
+    }
+   else if (window.innerHeight + document.documentElement.scrollTop !==  //worked on desktop not on touch screen
+     document.documentElement.offsetHeight || isFetching) return;
     setIsFetching(true);
   }
 
-  const handleTouchEnd = (e) => {
-   // if (e.target.tagName !== "a") {
-       // e.preventDefault(); 
-        handleScroll();
-      //}
-      // else {
-      //   console.log("this makes me a click event, most likely")
-      // }
-  }
+  //For touch event, didnt work
+  // const handleTouchEnd = () => {
+  //   if (e.target.tagName !== "a") {
+  //       e.preventDefault(); 
+  //       handleScroll();
+  //     }
+  //      else {
+  //        console.log("this makes me a click event, most likely")
+  //      }
+  // }
 
   return [isFetching, setIsFetching];
 };
